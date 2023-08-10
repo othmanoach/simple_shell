e0fb7250a766f59f15ea9cf64da538b0cfe2c942
Sami <samiztsouhailsami@gmail.com>
2023-08-10 11:08:28 +0100

    fst commit

---
+++ shell.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ * @argc: Number of arguments
+ * @argv: Arguments
+ * @env: Environment
+ *
+ * Return: 0 on success, 1 on failure
+ */
+int main(int argc, char **argv, char **env)
+{
+	printf ("hello world\n");
+}

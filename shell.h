89037d8400c07f99df1aaeade98a90423fe8c90b
TristanT8 <samiztsouhailsami@gmail.com>
2023-08-12 12:22:11 +0100

    dn

--- shell.h
+++ shell.h
@@ -14,15 +14,14 @@
 
 extern char **env;
 
-/* String functions */
-int _strlen(char *s);
-char *_strdup(char *str);
+/* ------------String functions--------------------- */
+
+
+/* calculates the length of a string */
+int _strlen(char *str);
+
+/* concatenate two strings */
 char *_strcat(char *dest, char *src);
-int _strcmp(char *s1, char *s2);
-char *_strchr(char *s, char c);
-char *_strtok(char *str, char *delim);
-int _strncmp(char *s1, char *s2, size_t n);
-char *_strncpy(char *dest, char *src, size_t n);
 
 /* print functions */
 void _puts(char *str);

aa56d21cd6a622d649aa29de11c86c21531ae9c7
Sami <samiztsouhailsami@gmail.com>
2023-08-12 12:29:08 +0100

    update header

--- shell.h
+++ shell.h
@@ -18,12 +18,39 @@ extern char **env;
 
 
 /* calculates the length of a string */
-int _strlen(char *str);
+int _strlen(char *str); 
 
 /* concatenate two strings */
 char *_strcat(char *dest, char *src);
 
-/* print functions */
-void _puts(char *str);
+/* compare two strings */
+int _strcmp(char *s1, char *s2);
+
+/* compare two strings */
+int _strncmp(char *s1, char *s2, size_t n);
+
+/* copies a string */
+char *_strcpy(char *dest, char *src);
+
+/* copies a string */
+char *_strncpy(char *dest, char *src, int n);
+
+/* returns a pointer to a new string which is a duplicate of the string str */
+char *_strdup(char *str);
+
+/* write a character to stdout */
 int _putchar(char c);
+
+/* write a string to stdout */
+void _puts(char *str);
+
+
+/* --------------String functions------------------- */
+
+/* --------------Memory functions------------------- */
+
+/* copies a memory area */
+char *_memcpy(char *dest, char *src, unsigned int n); 
+
+/* --------------Memory functions------------------- */
 

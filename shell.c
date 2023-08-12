b3c0587b7ad49e94b52d7cde6a2d66dd009fb958
TristanT8 <samiztsouhailsami@gmail.com>
2023-08-12 14:06:18 +0100

    test

--- shell.c
+++ shell.c
@@ -9,52 +9,47 @@ int shell_helper(char *input);
  */
 int main(void)
 {
-	bool running = true;
-	char input[MAX_INPUT_LENGTH];
+    bool running = true;
+    char input[MAX_INPUT_LENGTH];
 
-	while (running)
-	{
-		if (isatty(STDIN_FILENO))
-			write(STDOUT_FILENO, "#shell_dial_sb3$ ", 17);
-		else
-			running = false;
+    while (running)
+    {
+        if (isatty(STDIN_FILENO))
+            write(STDOUT_FILENO, "#shell_dial_sb3$ ", 17);
+        else
+            running = false;
 
-		if (fgets(input, MAX_INPUT_LENGTH, stdin) == NULL)
-		{
-			break;
-		}
-		if (input[0] == '\n')
-			continue;
+        if (fgets(input, MAX_INPUT_LENGTH, stdin) == NULL)
+        {
+            break;
+        }
+        if (input[0] == '\n')
+            continue;
 
-		if (_strcmp(input, "exit\n") == 0)
-		{
-			running = false;
-			continue;
-		}
-		shell_helper(input);
-	}
-	return (0);
+        if (strcmp(input, "exit\n") == 0)
+        {
+            running = false;
+            continue;
+        }
+        shell_helper(input);
+    }
+    return (0);
 }
-/**
- * shell_helper - function that executes the shell
- * @input: input from the user
- *
- * Return: Always 0.
- */
+
 int shell_helper(char *input)
 {
-	char *args[MAX_INPUT_LENGTH];
-	char *token = strtok(input, " ");
-	int i = 0;
-	while (token != NULL)
-	{
-		args[i++] = token;
-		token = strtok(NULL, " ");
-	}
-	args[i] = NULL;
-	if (execvp(args[0], args) == -1)
-	{
-		perror("execvp");
-	}
-	return (0);
+    char *args[MAX_INPUT_LENGTH];
+    char *token = strtok(input, " ");
+    int i = 0;
+    while (token != NULL)
+    {
+        args[i++] = token;
+        token = strtok(NULL, " ");
+    }
+    args[i] = NULL;
+    if (system(args) != 0)
+    {
+        perror("shell_dial_sb3");
+    }
+    return (0);
 }

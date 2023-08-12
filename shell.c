d0d1f69206c3d883de1462d877a1d5f01173344c
TristanT8 <samiztsouhailsami@gmail.com>
2023-08-12 13:08:23 +0100

    dd

--- shell.c
+++ shell.c
@@ -1,35 +1,31 @@
 #include "shell.h"
 
-/**
- * main - Entry point
- * @argc: Number of arguments
- * @argv: Arguments
- * @env: Environment
- *
- * Return: 0 on success, 1 on failure
- */
 int main(void)
 {
-	bool running = true;
-	char input[1024];
-	while (running)
-	{
-		if (isatty(STDIN_FILENO))
-			write(STDOUT_FILENO, "#shell_dial_sb3$", 16);
-		else
-			running = false;
-		
-		if (fgets(input, 1024, stdin) == NULL)
-		{
-			break;
-		}
-		if (input[0] == '\n')
-			continue;
-		if (_strcmp(input, "exit\n") == 0)
-		{
-			running = false;
-			continue;
-		}
-	}
-	return (0);
+    bool running = true;
+    char input[MAX_INPUT_LENGTH];
+    
+    while (running)
+    {
+        if (isatty(STDIN_FILENO))
+            write(STDOUT_FILENO, "#shell_dial_sb3$ ", 17);
+        else
+            running = false;
+
+        if (fgets(input, MAX_INPUT_LENGTH, stdin) == NULL)
+        {
+            break;
+        }
+        
+        if (input[0] == '\n')
+            continue;
+        
+        if (strcmp(input, "exit\n") == 0)
+        {
+            running = false;
+            continue;
+        }
+    }
+    
+    return 0;
 }

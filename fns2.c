dccf09bc8696dffac92801ca73532ad893d435fe
TristanT8 <samiztsouhailsami@gmail.com>
2023-08-13 16:22:52 +0100

    simple_shell\fns2.c update

--- fns2.c
+++ fns2.c
@@ -8,56 +8,48 @@
  */
 int executeCommand(char **arguments, char *buffer)
 {
-    pid_t processID;
-    char *commandPath = NULL;
-    int exitCode = handleBuiltIn(arguments, buffer);
+	pid_t processID;
+	char *commandPath = NULL;
+	int exitCode = handleBuiltIn(arguments, buffer);
+	int i = 0;
 
-    if (exitCode == -1)
-    {
-        commandPath = getCommandPath(arguments[0]);
-        if (!commandPath)
-        {
-            _puts("Not found");
-            return (2);
-        }
-        processID = fork();
-        if (processID == -1)
-        {
-            perror("fork");
-            exit(EXIT_FAILURE);
-        }
-        if (processID == 0)
-        {
-            int out_fd = dup(STDOUT_FILENO);
-            close(STDOUT_FILENO);
-            if (dup2(out_fd, STDOUT_FILENO) == -1)
-            {
-                perror("dup2");
-                exit(EXIT_FAILURE);
-            }
+	if (exitCode == -1)
+	{
+		commandPath = getCommandPath(arguments[0]);
+		if (!commandPath)
+		{
+			_puts("Not found");
+			return (2);
+		}
+		processID = fork();
+		if (processID == -1)
+		{
+			perror("fork");
+			exit(EXIT_FAILURE);
+		}
+		if (processID == 0)
+		{
+			int out_fd = dup(STDOUT_FILENO);
+			close(STDOUT_FILENO);
+			if (dup2(out_fd, STDOUT_FILENO) == -1)
+			{perror("dup2");
+			exit(EXIT_FAILURE);}
 
-            if (execve(commandPath, arguments, environ) == -1)
-            {
-                perror("execve");
-                freeArguments(arguments);
-                free(buffer);
-                exit(2);
-            }
-        }
-        else
-        {
-            int status;
-            while (i < 3) {
-                waitpid(processID, &status, 0);
-                if (WIFEXITED(status) && WEXITSTATUS(status) != 0)
-                    exitCode = 2;
-                i++;
-            }
-        }
-        if (_strcmp(commandPath, arguments[0]) != 0)
-            free(commandPath);
-    }
-    return exitCode;
+			if (execve(commandPath, arguments, environ) == -1)
+			{perror("execve");
+				freeArguments(arguments);
+				free(buffer);
+				exit(2);}
+		}
+		else{int status;
+			while (i < 3) {waitpid(processID, &status, 0);
+				if (WIFEXITED(status) && WEXITSTATUS(status) != 0)
+					exitCode = 2;
+				i++;}}
+		if (_strcmp(commandPath, arguments[0]) != 0)
+			free(commandPath);
+	}
+	return exitCode;
 }
 
 /**
@@ -67,15 +59,15 @@ int executeCommand(char **arguments, char *buffer)
  */
 char *getEnvVariable(char *name)
 {
-    int i = 0;
-    ssize_t len = _strlen(name);
+	int i = 0;
+	ssize_t len = _strlen(name);
 
-    for (; environ[i]; i++)
-    {
-        if (strncmp(environ[i], name, len) == 0 && environ[i][len] == '=')
-            return (&environ[i][len + 1]);
-    }
-    return (NULL);
+	for (; environ[i]; i++)
+	{
+		if (strncmp(environ[i], name, len) == 0 && environ[i][len] == '=')
+			return (&environ[i][len + 1]);
+	}
+	return (NULL);
 }
 
 /**
@@ -85,45 +77,45 @@ char *getEnvVariable(char *name)
  */
 char *getCommandPath(char *command)
 {
-    char *path, *pathCopy, *pathToken, *filePath;
-    int commandLength, directoryLength;
-    struct stat buffer;
+	char *path, *pathCopy, *pathToken, *filePath;
+	int commandLength, directoryLength;
+	struct stat buffer;
 
-    path = getEnvVariable("PATH");
-    if (path)
-    {
-        pathCopy = _strdup(path);
-        commandLength = _strlen(command);
-        pathToken = strtok(pathCopy, ":");
+	path = getEnvVariable("PATH");
+	if (path)
+	{
+		pathCopy = _strdup(path);
+		commandLength = _strlen(command);
+		pathToken = strtok(pathCopy, ":");
 
-        while (pathToken)
-        {
-            directoryLength = _strlen(pathToken);
-            filePath = malloc(commandLength + directoryLength + 2);
-            _strcpy(filePath, pathToken);
-            _strcat(filePath, "/");
-            _strcat(filePath, command);
-            _strcat(filePath, "\0");
-            if (stat(filePath, &buffer) == 0)
-            {
-                free(pathCopy);
-                return (filePath);
-            }
-            else
-            {
-                free(filePath);
-                pathToken = strtok(NULL, ":");
-            }
-        }
-        free(pathCopy);
-        if (stat(command, &buffer) == 0)
-        {
-            return (command);
-        }
-        return (NULL);
-    }
+		while (pathToken)
+		{
+			directoryLength = _strlen(pathToken);
+			filePath = malloc(commandLength + directoryLength + 2);
+			_strcpy(filePath, pathToken);
+			_strcat(filePath, "/");
+			_strcat(filePath, command);
+			_strcat(filePath, "\0");
+			if (stat(filePath, &buffer) == 0)
+			{
+				free(pathCopy);
+				return (filePath);
+			}
+			else
+			{
+				free(filePath);
+				pathToken = strtok(NULL, ":");
+			}
+		}
+		free(pathCopy);
+		if (stat(command, &buffer) == 0)
+		{
+			return (command);
+		}
+		return (NULL);
+	}
 
-    return (NULL);
+	return (NULL);
 }
 
 /**
@@ -133,21 +125,21 @@ char *getCommandPath(char *command)
  */
 void splitInput(char *inputBuffer, char **tokenArray)
 {
-    size_t tokenLength;
-    const char *delimiter = " \t\n";
-    char *token = NULL;
-    int count = 0;
-    char *inputCopy = NULL;
+	size_t tokenLength;
+	const char *delimiter = " \t\n";
+	char *token = NULL;
+	int count = 0;
+	char *inputCopy = NULL;
 
-    inputCopy = _strdup(inputBuffer);
-    token = strtok(inputCopy, delimiter);
-    for (count = 0; token; count++)
-    {
-        tokenLength = _strlen(token);
-        tokenArray[count] = malloc(sizeof(char *) * tokenLength);
-        _strncpy(tokenArray[count], token, tokenLength + 1);
-        token = strtok(NULL, delimiter);
-    }
-    free(inputCopy);
+	inputCopy = _strdup(inputBuffer);
+	token = strtok(inputCopy, delimiter);
+	for (count = 0; token; count++)
+	{
+		tokenLength = _strlen(token);
+		tokenArray[count] = malloc(sizeof(char *) * tokenLength);
+		_strncpy(tokenArray[count], token, tokenLength + 1);
+		token = strtok(NULL, delimiter);
+	}
+	free(inputCopy);
 }
 

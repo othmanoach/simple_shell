ee3847b2a32d0538f63e8d5bc2f5996b74b913ab
othmanoach <133314105+othmanoach@users.noreply.github.com>
2023-08-22 16:43:19 +0100

    fixed

--- func11.c
+++ func11.c
@@ -1,17 +1,17 @@
 #include "shell.h"
 
-/**s
- * _strcspn - get the length of a prefix substring.s
- * @prmString: the string to searchs
- * @prmDeny: characters to denys
+/**
+ * _strcspn - get the length of a prefix substring.
+ * @prmString: the string to search
+ * @prmDeny: characters to deny
  * Return: a pointer to the first occurrence of the character c
  *         in the string s, or NULL if the character is not found
  */
-unsigned int _strcspn(char *prmString, char *prmDeny)s
+unsigned int _strcspn(char *prmString, char *prmDeny)
 {
 	unsigned int sLoop, dLoop;
 
-	for (dLoop = 0; prmDeny[dLoop] != '\0'; dLoop++)s
+	for (dLoop = 0; prmDeny[dLoop] != '\0'; dLoop++)
 	{
 		for (sLoop = 0; prmDeny[dLoop] != prmString[sLoop]; sLoop++)
 			if ('\0' == prmString[sLoop])

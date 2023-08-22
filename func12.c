6f6c4556321a198a48f9fbab6f9466ad4b2b03eb
othmanoach <othmanoach@gmail.com>
2023-08-22 08:24:13 -0700

    func12 betty

--- func12.c
+++ func12.c
@@ -2,7 +2,7 @@
 
 /**
  * _strtow - splits a string into words
- * @prmString: char pointer
+ * @prmString: the char pointer
  * @prmSeparators: separators
  * @prmEscapeSeparators: escaping separators
  * Return: word array
